28a21f3a62bde87fe5ea23834fe45d77f591709f
waniAEbro <wani43bro@students.unnes.ac.id>
2023-09-12 09:59:26 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,10 @@ Route::get('/', function () {
 });
 
 Route::resource("products", ProductController::class);
+Route::resource("components", ProductController::class);
+Route::resource("categories", ProductController::class);
+Route::resource("sales", ProductController::class);
+Route::resource("suppliers", ProductController::class);
+Route::resource("puchases", ProductController::class);
+Route::resource("productions", ProductController::class);
+Route::resource("customers", ProductController::class);
