0dc7255fed59d882ba5ebdce45ef6e139c7e4b18
rasikhkp <raxiest@gmail.com>
2023-09-12 10:13:23 +0700

    Konfigurasi TailwindCSS

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/app', function () {
+    return view('app');
+});
+
 Route::resource("products", ProductController::class);
 Route::resource("components", ProductController::class);
 Route::resource("categories", ProductController::class);
