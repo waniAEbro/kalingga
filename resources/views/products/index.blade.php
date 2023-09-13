d7297e0bdfa4b5224e878e6a102c09d260a87797
waniAEbro <wani43bro@students.unnes.ac.id>
2023-09-14 03:16:11 +0700

    update index products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -41,12 +41,22 @@
                                 </svg></a>
                         </div>
                     </th>
+                    <th scope="col" class="px-6 py-3">
+                        <div class="flex items-center">
+                            Harga
+                            <a href="#" @click="sortByColumn"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path
+                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                </svg></a>
+                        </div>
+                    </th>
                     <th>
                         Actions
                     </th>
                 </tr>
             </thead>
-            <tbody x-ref="tbody">
+            <tbody x-ref="tbody" id="products">
                 @foreach ($products as $product)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -60,6 +70,8 @@
                         </td>
                         <td class="px-6 py-4">
                             {{ $product->rfid }}
+                        </td>
+                        <td class="px-6 py-4 price">
                         </td>
                         <td class="flex gap-2 py-4">
                             <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">
@@ -104,3 +116,18 @@
         </table>
     </div>
 @endsection
+@push('script')
+    <script>
+        let products = {!! $products !!}
+
+        let products_element = document.querySelectorAll('#products tr')
+
+        products_element.forEach((element, index) => {
+            let price = products[index].components.reduce((total, component) => {
+                return total + (component.price_per_unit * component.pivot.quantity)
+            }, 0)
+
+            element.querySelector('.price').innerText = toRupiah(price)
+        });
+    </script>
+@endpush
