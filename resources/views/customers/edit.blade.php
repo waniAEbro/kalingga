a6f80959983f3237c6f22814c0f6db1214af70da
Baihaq43 <imambaihaq@students.unnes.ac.id>
2023-09-16 22:03:52 +0700

    FIX views Customers

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1 class="text-lg font my-7 font-[500]">Create Customer</h1>
+    <h1 class="text-lg font my-7 font-[500]">Edit Customer</h1>
 
-    <x-create-input-field :action="'suppliers'" :items="$customers">
-        <x-input-text :name="'name'" :label="'Supplier Name'" :placeholder="'name'" :value="$customers->name" class="mb-3" />
-        <x-input-text :name="'email'" :label="'Email'" :placeholder="'email'" :value="$customers->email" class="mb-3" />
+    <x-edit-input-field :action="'customers'" :items="$customers">
+        <x-input-text :name="'name'" :label="'Customer Name'" :placeholder="'name'" :value="$customers->name" type="'text'" class="mb-3" />
+        <x-input-text :name="'email'" :label="'Email'" :placeholder="'email'" :value="$customers->email" type="'email'" class="mb-3" />
         <x-input-text :name="'phone'" :label="'Phone'" :placeholder="'phone'" :type="'number'" :value="$customers->phone"
             class="mb-3" />
         <x-input-textarea :name="'address'" :label="'Address'" :placeholder="'address'" :value="$customers->address" class="mb-3" />
         <x-input-text :name="'code'" :label="'Code'" :placeholder="'code'" :type="'number'" :value="$customers->code"
             class="mb-3" />
-    </x-create-input-field>
+    </x-edit-input-field>
+
     {{-- <div x-data="data()" class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <form method="post" action="/customers/{{ $customers->id }}">
             @csrf
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -14,7 +14,7 @@
                     <div class="flex items-center justify-center gap-3 border-l h-7 border-slate-200">
                         <a href="/customers/{{ $customer->id }}/edit" class="flex items-center gap-1 text-slate-600"><span
                                 class="text-lg"><ion-icon name="create-outline"></ion-icon></span>Edit</a>
-                        <form action="/customers/{{ $customer->id }}">
+                        <form action="/customers/{{ $customer->id }}" method="POST">
                             @csrf
                             @method('delete')
                             <button class="flex items-center gap-1 text-red-700"><span class="text-lg"><ion-icon
