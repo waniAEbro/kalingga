bb998a53a870f3f48d6d7a209e7ad078d9bb62ed
Baihaq43 <imambaihaq@students.unnes.ac.id>
2023-09-17 13:31:26 +0700

    EDIT(?) view component(index, create, edit)

--- resources/views/component/create.blade.php
+++ resources/views/component/create.blade.php
@@ -6,6 +6,7 @@
     <x-create-input-field :action="'components'">
         <x-input-text :name="'name'" :label="'Component Name'" :placeholder="'kayu'" :type="'text'" class="mb-3" />
         <x-input-text :name="'unit'" :label="'Component Unit'" :placeholder="'m'" class="mb-3" />
-        <x-input-text :name="'price_per_unit'" :label="'Component Price'" :placeholder="'1000'" />
+        <x-input-text :name="'price_per_unit_sell'" :label="'Sell\'s Price Per Unit'" :placeholder="'1000'" />
+        <x-input-text :name="'price_per_unit_buy'" :label="'Buy\'s Price Per Unit'" :placeholder="'1000'" />
     </x-create-input-field>
 @endsection
--- resources/views/component/edit.blade.php
+++ resources/views/component/edit.blade.php
@@ -6,6 +6,7 @@
     <x-edit-input-field :action="'components'" :items="$componentedit">
         <x-input-text :name="'name'" :label="'Component Name'" :placeholder="'kayu'" :value="$componentedit->name" :type="'text'" class="mb-3" />
         <x-input-text :name="'unit'" :label="'Component Unit'" :placeholder="'m'" :value="$componentedit->unit" class="mb-3" />
-        <x-input-text :name="'price_per_unit'" :label="'Component Price'" :placeholder="'1000'" :value="$componentedit->price_per_unit" />
+        <x-input-text :name="'price_per_unit_sell'" :label="'Sell\'s Price Per Unit'" :placeholder="'1000'" :value="$componentedit->price_per_unit_sell" />
+        <x-input-text :name="'price_per_unit_buy'" :label="'Buy\'s Price Per Unit'" :placeholder="'1000'" :value="$componentedit->price_per_unit_buy" />
     </x-edit-input-field>
 @endsection
