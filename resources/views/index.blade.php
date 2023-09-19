2a62093075fb25396d4e969cb900e048e9133a0f
rasikhkp <raxiest@gmail.com>
2023-09-19 14:53:36 +0700

    build lg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.layout')
 
 @section('content')
-    <x-create-input-field :action="'components'">
-
-        <x-select-with-search-ori />
-
-    </x-create-input-field>
+    Ini dashboard
 @endsection
