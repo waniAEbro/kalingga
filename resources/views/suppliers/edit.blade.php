adb71d0d1222eabe445bd65c279af24bc0bd0c34
waniAEbro <wani43bro@students.unnes.ac.id>
2023-09-20 21:08:15 +0700

    update bug

--- resources/views/suppliers/edit.blade.php
+++ resources/views/suppliers/edit.blade.php
@@ -9,7 +9,7 @@
         <x-input-text :name="'email'" :label="'Email'" :placeholder="'email'" :value="$suppliers->email" :type="'email'"
             class="mb-3" />
         <x-input-text :name="'phone'" :label="'Phone'" :placeholder="'phone'" :value="$suppliers->phone" class="mb-3" />
-        <x-input-text :name="'phone'" :label="'Code'" :placeholder="'Code'" :value="$suppliers->code" class="mb-3" />
+        <x-input-text :name="'code'" :label="'Code'" :placeholder="'Code'" :value="$suppliers->code" class="mb-3" />
         <x-input-textarea :name="'address'" :label="'Address'" :placeholder="'address'" :value="$suppliers->address" />
     </x-edit-input-field>
 @endsection
