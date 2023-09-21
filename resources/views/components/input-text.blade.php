bd1c94e98802f8b58f1ac1a4b548d8a561f39e4b
waniAEbro <wani43bro@students.unnes.ac.id>
2023-09-21 11:23:09 +0700

    update input number

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,6 +1,6 @@
 @props(['label' => '', 'name' => '', 'placeholder' => '', 'value' => '', 'type' => 'text'])
 
 <label for="{{ $name }}" class="block text-sm">{{ $label }}</label>
-<input id="{{ $name }}" name="{{ $name }}"
+<input id="{{ $name }}" name="{{ $name }}" step="0.001"
     {{ $attributes->merge(['class' => 'py-2 px-3 w-full mt-2 rounded text-sm border outline-none focus:outline focus:outline-4 focus:outline-offset-0 focus:outline-slate-300 transition-all duration-100']) }}
     type="{{ $type }}" placeholder="{{ $placeholder }}" value="{{ $value }}">
