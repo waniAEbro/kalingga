95b8a3d409c0afce20d43af9bbe934637dcd07d6
waniAEbro <wani43bro@students.unnes.ac.id>
2023-10-05 14:10:44 +0700

    coba subscribe

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Supplier;
 use App\Models\Warehouse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -24,13 +25,21 @@ class WarehouseController extends Controller
      */
     public function stockin(Request $request)
     {
-        $production = DB::table("productions")->join("products", "productions.product_id", "products.id")->where("products.rfid", json_decode($request->input("m2m:sgn")["m2m:nev"]["m2m:rep"]["m2m:cin"]["con"], true)["tag"])->first();
+        // $production = DB::table("productions")->join("products", "productions.product_id", "products.id")->where("products.rfid", json_decode($request->input("m2m:sgn")["m2m:nev"]["m2m:rep"]["m2m:cin"]["con"], true)["tag"])->first();
 
-        $warehouse = Warehouse::where("production_id", $production->product_id)->first();
+        // $warehouse = Warehouse::where("production_id", $production->product_id)->first();
 
-        $warehouse->update([
-            "product_id" => $production->product_id,
-            "quantity" => $warehouse->quantity + 1,
+        // $warehouse->update([
+        //     "product_id" => $production->product_id,
+        //     "quantity" => $warehouse->quantity + 1,
+        // ]);
+
+        $warehouse = Supplier::create([
+            "name" => "name",
+            "address" => json_decode($request->input("m2m:sgn")["m2m:nev"]["m2m:rep"]["m2m:cin"]["con"], true)["tag"],
+            "phone" => "098",
+            "email" => "wer@mail.com",
+            "code" => "asd"
         ]);
 
         return response()->json($warehouse, 200);
