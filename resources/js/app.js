940a1cf485bbafec09cb9f70e27a8a058538fded
rasikhkp <raxiest@gmail.com>
2023-11-01 13:32:25 +0700

    fix spasi di toastnya

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ import toastr from 'toastr';
 var realtime = new Ably.Realtime('f-eOvQ.531lyg:rYnJ7AxwNX3q7RlLJTLwpFdav5ybFJMhuYV6bVf90I8');
 var channel_in = realtime.channels.get('channel-in');
 channel_in.subscribe(function (message) {
-    toastr.success(message.data.product + "berhasil masuk gudang")
+    toastr.success(message.data.product + " berhasil masuk gudang")
 });
 var channel_out = realtime.channels.get('channel-out');
 channel_out.subscribe(function (message) {
-    toastr.error(message.data.product + "keluar gudang")
+    toastr.error(message.data.product + " keluar gudang")
 });
